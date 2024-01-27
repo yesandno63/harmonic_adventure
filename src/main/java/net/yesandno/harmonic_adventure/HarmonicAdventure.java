e382d7832a1950a6666e64828d7484bb014a6f52
yesandno63 <77505022+yesandno63@users.noreply.github.com>
2024-01-27 21:42:37 +0100

    +sapphire block +steel block +trad

--- src/main/java/net/yesandno/harmonic_adventure/HarmonicAdventure.java
+++ src/main/java/net/yesandno/harmonic_adventure/HarmonicAdventure.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.yesandno.harmonic_adventure.block.ModBlocks;
 import net.yesandno.harmonic_adventure.item.ModCreativeModTabs;
 import net.yesandno.harmonic_adventure.item.ModItems;
 import org.slf4j.Logger;
@@ -29,6 +30,7 @@ public HarmonicAdventure() {
         ModCreativeModTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
--- src/main/java/net/yesandno/harmonic_adventure/item/ModCreativeModTabs.java
+++ src/main/java/net/yesandno/harmonic_adventure/item/ModCreativeModTabs.java
@@ -8,6 +8,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.yesandno.harmonic_adventure.HarmonicAdventure;
+import net.yesandno.harmonic_adventure.block.ModBlocks;
 
 public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB_DEFERRED_REGISTER =
@@ -19,6 +20,11 @@ public class ModCreativeModTabs {
                     .displayItems((itemDisplayParameters, output) -> {
                        output.accept(ModItems.STEEL.get());
                        output.accept(ModItems.STEEL_NUGGET.get());
+                       output.accept(ModItems.BLOOD_TEAR.get());
+                       output.accept(ModItems.SAHPPIRE.get());
+
+                       output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                       output.accept(ModBlocks.STEEL_BLOCK.get());
                     })
                     .build());
 
